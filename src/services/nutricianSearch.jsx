7756c4ee4cc0fd52763cd3bba995b9af58272eac
Petri Asikainen <petri.asikainen@gmail.com>
2020-02-16 20:30:58 +0200

    callback funktio nimetty paremmin

--- src/services/nutricianSearch.jsx
+++ src/services/nutricianSearch.jsx
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-export default function NutricianSearchAPICall(profileDataUpdateFunction, searchParams)
+export default function NutricianSearchAPICall(callBack, searchParams)
 {
     axios.get(`http://localhost:3001/nutricians/?q=${searchParams ? searchParams : ''}`)
     .then(response => {
         console.log("Nutrician API Response: ", response.data);
-        profileDataUpdateFunction(response.data);
+        callBack(response.data);
     })
     .catch(error => {
         console.log(error);
