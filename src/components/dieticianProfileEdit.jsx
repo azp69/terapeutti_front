6ac41d6369a6e4106ab13d010108fb8adfd46dd3
Petri Asikainen <petri.asikainen@gmail.com>
2020-04-18 17:47:18 +0300

    Toimipaikan valintaa fiksi

--- src/components/dieticianProfileEdit.jsx
+++ src/components/dieticianProfileEdit.jsx
@@ -21,17 +21,17 @@ export default function DieticianProfileEdit({
 				</h5>
 				<hr />
 
-				<div class="container">
+				<div className="container">
 					<div className="row">
-						<div class="col-4 text-center">
+						<div className="col-4 text-center">
 							<h2>kuva</h2>
 						</div>
 						{/*ekan rivin tiedot*/}
-						<div class="col-8">
-							<div class="row">
-								<div class="col form-group">
+						<div className="col-8">
+							<div className="row">
+								<div className="col form-group">
 									<label>
-										<h8>Etunimi</h8>
+										<h6>Etunimi</h6>
 									</label>
 									<TextInput
 										value={profile.name}
@@ -40,15 +40,15 @@ export default function DieticianProfileEdit({
 									></TextInput>
 								</div>
 
-								<div class="col">
+								<div className="col">
 									<label>
-										<h8>Toimipaikka</h8>
+										<h6>Toimipaikka</h6>
 									</label>
 									<select
 										className="form-control"
 										id="Place"
 										onChange={dataChangeHandler}
-										defaultValue={profile.place}
+										value={profile.place}
 									>
 										<RenderPlaces place={profile.place} />
 									</select>
@@ -58,10 +58,10 @@ export default function DieticianProfileEdit({
 							<hr />
 
 							{/*toisen rivin tiedot*/}
-							<div class="row">
-								<div class="col">
+							<div className="row">
+								<div className="col">
 									<label>
-										<h8>Puhelin</h8>
+										<h6>Puhelin</h6>
 									</label>
 									<TextInput
 										value={profile.phone}
@@ -69,9 +69,9 @@ export default function DieticianProfileEdit({
 										onChange={dataChangeHandler}
 									></TextInput>
 								</div>
-								<div class="col">
+								<div className="col">
 									<label>
-										<h8>Koulutus</h8>
+										<h6>Koulutus</h6>
 									</label>
 									<TextInput
 										value={profile.education}
@@ -132,133 +132,9 @@ export default function DieticianProfileEdit({
 		console.log("place ", place);
 
 		const p = places.map((x) => {
-			if (place == x.place)
-				return <option selected="selected">{x.place}</option>;
-
-			return <option>{x.place}</option>;
+			return <option key={`place_${x.id}`}>{x.place}</option>;
 		});
 
 		return p;
 	}
 }
-
-/*
-
-<div className="col-sm-12 mt-5 card card-body bg-light">
-				<div className="py-5 my-3 px-5 text-center">
-					<form>
-						<h1>Omat tietosi</h1>
-						<div className="container">
-							<div className="row">
-								<div className="col-4 text-center">
-									<h2>kuva</h2>
-								</div>
-								<div className="col-8">
-									<div className="row">
-										<div className="col">
-											<label>
-												<h6>Nimi</h6>
-											</label>
-										</div>
-										<div className="col">
-											<label>
-												<h6>Toimipaikka</h6>
-											</label>
-										</div>
-									</div>
-									<div className="row">
-										<div className="col">
-											<label htmlFor="name">
-												<TextInput
-													value={dieticianData.name}
-													id="name"
-													onChange={handleChangeDieticianValues}
-												></TextInput>
-											</label>
-										</div>
-										<div className="col">
-											<label htmlFor="Place">
-												<select
-													className="form-control"
-													id="Place"
-													onChange={handleChangeDieticianValues}
-												>
-													<option>Helsinki</option>
-													<option>Vantaa</option>
-													<option>Espoo</option>
-													<option>Kuopio</option>
-													<option>Oulu</option>
-												</select>
-											</label>
-										</div>
-									</div>
-									<hr />
-									<div className="row">
-										<div className="col">
-											<label>
-												<h6>Puhelin</h6>
-											</label>
-										</div>
-										<div className="col">
-											<label>
-												<h6>Koulutus</h6>
-											</label>
-										</div>
-									</div>
-									<div className="row">
-										<div className="col">
-											<label htmlFor="puhelin">
-												<TextInput
-													value={dieticianData.phone}
-													id="phone"
-													onChange={handleChangeDieticianValues}
-												></TextInput>
-											</label>
-										</div>
-										<div className="col">
-											<label htmlFor="education">
-												<TextInput
-													value={dieticianData.education}
-													id="education"
-													onChange={handleChangeDieticianValues}
-												></TextInput>
-											</label>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div className="row text-left">
-								<div className="col-sm-12">
-									<hr />
-									<h5>Erikoisosaamiset</h5>
-								</div>
-								<ExpertiesList
-									experties={experties}
-									selectedExperties={dieticianData.expertises}
-									onChange={handleExpertiesChange}
-								/>
-							</div>
-							<div className="row">
-								<div className="col-sm-12">
-									<hr />
-									<button
-										type="button"
-										className="btn btn-primary mr-1"
-										onClick={handleSubmitData}
-									>
-										Tallenna
-									</button>
-									<button
-										type="button"
-										className="btn btn-danger ml-1"
-										onClick={handleCancel}
-									>
-										Peruuta
-									</button>
-								</div>
-							</div>
-						</div>
-					</form>
-				</div>
-			</div>
-*/
